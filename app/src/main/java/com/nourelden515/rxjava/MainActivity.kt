64dab55430ea42239b0ae9c4625939b47cc04a1e
Nour Elden Alaa <nourelden515@gmail.com>
2023-04-27 18:33:31 +0200

    enhance the code

--- app/src/main/java/com/nourelden515/rxjava/MainActivity.kt
+++ app/src/main/java/com/nourelden515/rxjava/MainActivity.kt
@@ -3,12 +3,9 @@ package com.nourelden515.rxjava
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.nourelden515.rxjava.databinding.ActivityMainBinding
-import org.greenrobot.eventbus.EventBus
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    // Create an event bus
-    val eventBus: EventBus = EventBus.getDefault()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
